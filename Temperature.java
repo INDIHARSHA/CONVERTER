84e231333a8b419cad7cb6ee43e20832cfb31986
INDIHARSHA <110756813+INDIHARSHA@users.noreply.github.com>
2025-10-01 21:00:13 +0530

    Update Temperature.java

--- Temperature.java
+++ Temperature.java
@@ -24,26 +24,34 @@ public class Temperature
         int k=sc.nextInt();
            if(n==1)
            {
+               System.out.println("Fahrenheit ---> Celsius");
+               
                System.out.println(Math.abs((k-32)/1.8));
             }
             if(n==2)
            {
+               System.out.println("Celsius ---> Fahrenheit");
                System.out.println(Math.abs((k*1.8)+32));
             }
             if(n==3)
            {
+               System.out.println("Kelvin ---> Celsius");
                System.out.println(Math.abs(k-273));
             }
             if(n==4)
            {
+               System.out.println("Kelvin ---> Fahrenheit");
                System.out.println(Math.abs((k*1.8)-459.67));
             }
             if(n==5)
            {
+               System.out.println("Celsius ---> Kelvin");
+               
                System.out.println(Math.abs(k+273));
             }
             if(n==6)
            {
+               System.out.println("Fahrenheit ---> Kelvin");
                System.out.println(Math.abs((k+ 459.67)*5/9));
             }
            
